f80d9681a4af5c29143a387797cf57f32f907bfe
obaidadil <obaidadil@hotmail.com>
2021-11-02 06:10:05 -0400

    final code

--- asserEqual.js
+++
@@ -1,3 +0,0 @@
-const assertEqual = function(actual, expected) {
-  actual === expected ? console.log(`🟩🟩🟩 Assertion Passed: ${actual} === ${expected}`) : console.log(`🟥🟥🟥 Assertion Failed: ${actual} !== ${expected}`);
-};
